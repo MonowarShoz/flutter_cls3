f64afd76c35e7056d635450b44ac7ad2f9c0b943
MonowarShoz <shozolhorn@gmail.com>
2022-08-21 18:11:41 +0600

    class 5th

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -6,13 +6,28 @@ import 'package:flutter_cls3/constants.dart';
 class SecondPage extends StatefulWidget {
   final String username;
   final String password;
-  const SecondPage({Key? key, required this.username, required this.password}) : super(key: key);
+  const SecondPage({Key? key, required this.username, required this.password})
+      : super(key: key);
 
   @override
   State<SecondPage> createState() => _SecondPageState();
 }
 
 class _SecondPageState extends State<SecondPage> {
+  String? textvalue;
+
+  final dummyData = ["Cat", "Dog", "Cow", "Tiger", "bird"];
+  final newDummyData = ["programming Language", "Java", "C", "c", "dart"];
+
+  final List<ListDataModel> list = [
+    ListDataModel('First Card', Colors.yellow),
+     ListDataModel('second Card', Colors.green),
+      ListDataModel('third Card', Colors.red),
+       ListDataModel('4th Card', Colors.amber),
+        ListDataModel('5th Card', Colors.black),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,11 +42,79 @@ class _SecondPageState extends State<SecondPage> {
           ],
         ),
       ),
-      body: Center(
-        child: Text(''),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Color.fromARGB(255, 207, 230, 240),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, top: 30),
+              child: SizedBox(
+                width: 200,
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Comment",
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (v) {
+                    setState(() {
+                      textvalue = v;
+                    });
+                  },
+                ),
+              ),
+            ),
+            textvalue == null
+                ? SizedBox.shrink()
+                : Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(textvalue!),
+                  ),
+            // Expanded(
+            //   child: ListView(
+            //     children: [
+            //       ...dummyData.map((item) {
+            //         return Text(item);
+            //       }).toList(),
+
+            //       // ...dummyData.map((item) {
+            //       //   return Text(item);
+            //       // }).toList(),
+            //     ],
+            //   ),
+            // ),
+            Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: list.length,
+                  itemBuilder: (context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 150,
+                        height: 40,
+                        child: Card(
+                          color: list[index].color,
+                          elevation: 3,
+                          child: Center(child: Text(list[index].title,style: TextStyle(color: Colors.white),)),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
+class ListDataModel {
+  final String title;
+  final Color color;
 
+  ListDataModel(this.title, this.color);
+}
